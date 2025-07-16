c81e0638c31711fb8e1855347d7ebf74b41a5908
mazode <mrmaazkhan191@gmail.com>
2025-07-16 21:31:16 +0300

    installed dependencies, defined middlewares and utils

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import "dotenv/config";
 import connectDB from "./db/index.js";
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server started on ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed", error);
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,9 @@
+const asyncHandler = () => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+export { asyncHandler };

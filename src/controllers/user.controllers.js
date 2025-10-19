d9ce687ada9c3781b6d9909465de433c8a5542ec
mazode <mrmaazkhan191@gmail.com>
2025-10-19 23:47:54 +0300

    add error handling for register controller

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,7 +1,12 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   const { username, email, password, refreshToken } = req.body;
+
+  if (fullName === "") {
+    throw new ApiError("name can't be empty");
+  }
 });
 
 export { registerUser };
